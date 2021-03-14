170e2d6174d4ec63e1c187d20e736ffb0cce25bd
Nrg51098 <gaikwadrnikhil@gmail.com>
2021-03-13 19:24:47 -0600

    finished the CRUD logic for the tags

--- homeiotapi/views/__init__.py
+++ homeiotapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .auth import register_user, login_user, get_current_user, is_current_user_admin
 from .users import UsersViewSet
 from .devices import DevicesViewSet
+from .tags import TagViewSet
